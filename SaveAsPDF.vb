2dcd9d08b4130d8d9737668023869b5db8a3f25b
matthaeus123 <60934578+matthaeus123@users.noreply.github.com>
2020-02-26 10:27:56 -0800

    Added disabled code for expansion to excel
    Macro needs to be exported or changed to work with excel.

--- SaveAsPDF.vb
+++ SaveAsPDF.vb
@@ -3,6 +3,7 @@ Sub savepdf()
 Dim oDoc As Document
 Dim strFname As String
 Dim strPath As String
+Dim strPathSP As String
 Set oDoc = ActiveDocument
 'define the format of the filename - with the year and time as a prefix
 'adding seconds to the time will ensure that each time you use the macro you will get
@@ -11,7 +12,7 @@ strFname = "BUY-Weekly Add-Drop " & _
             Format(Date, "mm.dd.yyyy")
 
 'define the folder location to save the document
-strPath = "C:\Users\matt.walker\Desktop\Test\"
+strPath = "\\bdshare\buyers\Add-Drop\WEEKLY ADD DROPS\2020\"
 With oDoc
     'print the document to the current printer
     '.PrintOut
@@ -21,4 +22,18 @@ With oDoc
     .SaveAs strPath & strFname & ".pdf", FileFormat:=wdFormatPDF
     
 End With
+
+'Dead Code to work into Excel
+'define the folder location to save the document in SharePoint
+'strPath2 = "\\bdshare\buyers\Add-Drop\WEEKLY ADD DROPS\2020\"
+'With oDoc
+    'print the document to the current printer
+    '.PrintOut
+    'save the document in Word docx format
+    '.SaveAs strPath2 & strFname & ".docx", FileFormat:=wdFormatDocumentDefault
+    'save the document as PDF format in the same folder
+    '.SaveAs strPath2 & strFname & ".pdf", FileFormat:=wdFormatPDF
+    
+End With
+
 End Sub
