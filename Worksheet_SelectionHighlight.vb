a3aae599bb8e7bbdcdf81f89c7e313f665d9857b
matthaeus123 <60934578+matthaeus123@users.noreply.github.com>
2020-02-26 09:18:29 -0800

    Update Worksheet_SelectionHighlight.vb

--- Worksheet_SelectionHighlight.vb
+++ Worksheet_SelectionHighlight.vb
@@ -1,5 +1,12 @@
 Private Sub Worksheet_SelectionChange(ByVal Target As Range)
-If Application.CutCopyMode = False Then
-Application.Calculate
-End If
+    If Target.Cells.Count > 1 Then Exit Sub
+    Application.ScreenUpdating = False
+    ' Clear the color of all the cells
+    Cells.Interior.ColorIndex = 0
+    With Target
+        ' Highlight the entire row and column that contain the active cell
+        .EntireRow.Interior.ColorIndex = 8
+        .EntireColumn.Interior.ColorIndex = 8
+    End With
+    Application.ScreenUpdating = True
 End Sub
