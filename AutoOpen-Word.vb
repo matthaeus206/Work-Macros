92404aca0a03316010e79228992d7c408f929c8e
matthaeus123 <60934578+matthaeus123@users.noreply.github.com>
2020-02-27 07:53:31 -0800

    Create AutoOpen-Word.vb

---
+++ AutoOpen-Word.vb
@@ -0,0 +1,26 @@
+Public Sub AutoOpen()
+
+Dim oDoc As Document
+Dim strFname As String
+Dim strPath As String
+Dim strPathSP As String
+Set oDoc = ActiveDocument
+'define the format of the filename - with the year and time as a prefix
+'adding seconds to the time will ensure that each time you use the macro you will get
+'a new copy.
+strFname = "BUY-Weekly Add-Drop " & _
+            Format(Date, "m.dd.yyyy")
+
+'define the folder location to save the document
+strPath = "\\bdshare\buyers\Add-Drop\WEEKLY ADD DROPS\2020\"
+With oDoc
+    'print the document to the current printer
+    '.PrintOut
+    'save the document in Word docx format
+    .SaveAs strPath & strFname & ".docx", FileFormat:=wdFormatDocumentDefault
+    'save the document as PDF format in the same folder
+    .SaveAs strPath & strFname & ".pdf", FileFormat:=wdFormatPDF
+    
+End With
+
+End Sub
