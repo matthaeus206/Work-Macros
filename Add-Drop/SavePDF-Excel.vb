83a0dbc85c43625c47b6b94f1e9674eeebdb1a6c
matthaeus123 <60934578+matthaeus123@users.noreply.github.com>
2020-07-15 11:37:41 -0700

    Update SavePDF-Excel.vb

--- Add-Drop/SavePDF-Excel.vb
+++ Add-Drop/SavePDF-Excel.vb
@@ -1,19 +1,19 @@
 Sub savepdf()
+Application.ScreenUpdating = False
+'Main routine to print out PDFs and send mail to storecomm and run everything.
     Dim strFname As String
     Dim strPath As String
     Dim strPathSP As String
     Dim oDoc As Worksheet
     Set oDoc = Sheets("Ready to Deploy")
-    
 strFname = "Weekly Add-Drop " & _
             Format(Date, "m.dd.yyyy")
-
 'define the folder location to save the document
 strPath = "\\bdshare\buyers\Add-Drop\WEEKLY ADD DROPS\2020\" & _
       strFname & ".pdf"
 
-Call FnOpeneWordDoc
 Call Mail_ActiveSheet
+'Call FnOpeneWordDoc
 
     'sFile = Application.DefaultFilePath & "\" & _
       'ActiveWorkbook.Name & ".pdf"
@@ -24,11 +24,33 @@ Call Mail_ActiveSheet
 'ActiveWorkbook.SaveAs Filename:=strPath & "\" &
       'strFname & ".xlsx"
     With Sheets("Ready to Deploy").PageSetup
+        .LeftMargin = Application.InchesToPoints(0.7)
+        .RightMargin = Application.InchesToPoints(0.7)
+        .TopMargin = Application.InchesToPoints(0.75)
+        .BottomMargin = Application.InchesToPoints(0.75)
+        .HeaderMargin = Application.InchesToPoints(0.3)
+        .FooterMargin = Application.InchesToPoints(0.3)
+        .PrintHeadings = False
+        .PrintGridlines = False
+        .PrintComments = xlPrintNoComments
+        .PrintQuality = 600
+        .CenterHorizontally = False
+        .CenterVertically = False
         .Orientation = xlLandscape
+        .Draft = False
+        .PaperSize = xlPaperLetter
+        .FirstPageNumber = xlAutomatic
+        .Order = xlDownThenOver
+        .BlackAndWhite = False
+        .Zoom = False
         .FitToPagesWide = 1
-        .FitToPagesTall = 1
+        .PrintErrors = xlPrintErrorsDisplayed
+        .OddAndEvenPagesHeaderFooter = False
+        .DifferentFirstPageHeaderFooter = False
+        .ScaleWithDocHeaderFooter = True
+        .AlignMarginsHeaderFooter = True
     End With
-
+      
     Sheets("Ready to Deploy").ExportAsFixedFormat Type:=xlTypePDF, _
       Filename:=strPath, Quality:=xlQualityStandard, _
       IncludeDocProperties:=True, IgnorePrintAreas:=False, _
@@ -50,5 +72,4 @@ Sheets("Ready to Deploy").Copy
         , Quality:=xlQualityStandard, IncludeDocProperties:=True, IgnorePrintAreas _
         :=False, OpenAfterPublish:=False
 Call Mail_storecomm
-
 End Sub
