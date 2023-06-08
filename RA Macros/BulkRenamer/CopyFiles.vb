503ee9821c344d7df29aa31d2b30f68f178d85f9
Matt <60934578+matthaeus206@users.noreply.github.com>
2023-06-08 12:25:29 -0700

    Update CopyFiles.vb

--- RA Macros/BulkRenamer/CopyFiles.vb
+++ RA Macros/BulkRenamer/CopyFiles.vb
@@ -7,56 +7,39 @@ Sub CopyFiles()
     Dim i As Long
     Dim notFoundList As String
     Dim fileExtension As String
-    Dim batchSize As Long
-    Dim filesToCopy As Collection
-    Dim file As Variant
     
     ' Get input values from user
     sourceFolder = InputBox("Enter source folder path:")
     destFolder = InputBox("Enter destination folder path:")
     fileExtension = InputBox("Enter file extension:")
-    batchSize = InputBox("Enter batch size (recommended: 1000):")
     Set rng = Application.InputBox("Select cells with search terms:", Type:=8)
     
-    ' Disable alerts and screen updating to prevent popups and improve performance
-    Application.DisplayAlerts = False
-    Application.ScreenUpdating = False
+    ' Create FileSystemObject
+    Dim fso As Object
+    Set fso = CreateObject("Scripting.FileSystemObject")
     
-    ' Initialize collection for files to copy
-    Set filesToCopy = New Collection
+    ' Disable alerts to prevent popups
+    Application.DisplayAlerts = False
     
     ' Loop through each cell in selected range
     For Each cell In rng
         ' Loop through files in source folder
         filename = Dir(sourceFolder & "\*" & cell.Value & "*" & fileExtension)
+        If filename = "" Then
+            ' File not found, add to not found list
+            notFoundList = notFoundList & cell.Value & vbCrLf
+        Else
+            ' Copy file to destination folder
+            fso.CopyFile sourceFolder & "\" & filename, destFolder & "\" & filename
+            i = i + 1
+        End If
         Do While filename <> ""
-            ' Add file to collection
-            filesToCopy.Add sourceFolder & "\" & filename
             filename = Dir()
         Loop
     Next cell
     
-    ' Loop through files in batches and copy them to destination folder
-    i = 0
-    For Each file In filesToCopy
-        FileCopy file, destFolder & "\" & GetFileNameFromPath(file)
-        i = i + 1
-        
-        ' Check if batch size limit is reached
-        If i Mod batchSize = 0 Then
-            ' Reset the counter and prevent interruption from user input
-            Application.EnableCancelKey = xlDisabled
-            DoEvents
-            Application.EnableCancelKey = xlInterrupt
-        
-            ' Update the progress to the user
-            Application.StatusBar = "Copying files: " & i & " files copied"
-        End If
-    Next file
-    
-    ' Enable alerts and screen updating
+    ' Enable alerts
     Application.DisplayAlerts = True
-    Application.ScreenUpdating = True
     
     ' Display message with number of files copied
     MsgBox i & " file(s) copied."
@@ -73,10 +56,6 @@ Sub CopyFiles()
         ws.Range("A2").Value = notFoundList
     End If
     
-    ' Clear the status bar
-    Application.StatusBar = ""
+    ' Release FileSystemObject
+    Set fso = Nothing
 End Sub
-
-Function GetFileNameFromPath(ByVal fullPath As String) As String
-    ' Retrieve the file name from the full file path
-    GetFileNameFromPath = Mid(fullPath, InStrRev(fullPath, "\")
