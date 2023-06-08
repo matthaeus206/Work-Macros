1c7b2ee6dac96b3ee30cb452cad9f23394687c42
Matt <60934578+matthaeus206@users.noreply.github.com>
2023-06-08 11:45:27 -0700

    Update CopyFiles.vb
    Added user defined file extension and sped up code

--- RA Macros/BulkRenamer/CopyFiles.vb
+++ RA Macros/BulkRenamer/CopyFiles.vb
@@ -6,34 +6,41 @@ Sub CopyFiles()
     Dim filename As String
     Dim i As Long
     Dim notFoundList As String
+    Dim fileExtension As String
     
     ' Get input values from user
     sourceFolder = InputBox("Enter source folder path:")
     destFolder = InputBox("Enter destination folder path:")
+    fileExtension = InputBox("Enter file extension:")
     Set rng = Application.InputBox("Select cells with search terms:", Type:=8)
     
     ' Disable alerts to prevent popups
     Application.DisplayAlerts = False
     
+    ' Disable screen updating
+    Application.ScreenUpdating = False
+    
     ' Loop through each cell in selected range
     For Each cell In rng
-        ' Loop through files in source folder
-        filename = Dir(sourceFolder & "\*" & cell.Value & "*" & ".psa")
-        If filename = "" Then
+        ' Construct the full file path
+        filename = sourceFolder & "\" & cell.Value & fileExtension
+        
+        ' Check if the file exists
+        If Dir(filename) <> "" Then
+            ' Copy file to destination folder
+            FileCopy filename, destFolder & "\" & cell.Value & fileExtension
+            i = i + 1
+        Else
             ' File not found, add to not found list
             notFoundList = notFoundList & cell.Value & vbCrLf
-        Else
-            ' Copy file to destination folder
-            fileCopy sourceFolder & "\" & filename, destFolder & "\" & filename
-            i = i + 1
         End If
-        Do While filename <> ""
-            filename = Dir()
-        Loop
     Next cell
     
     ' Enable alerts
     Application.DisplayAlerts = True
+    
+    ' Enable screen updating
+    Application.ScreenUpdating = True
     
     ' Display message with number of files copied
     MsgBox i & " file(s) copied."
