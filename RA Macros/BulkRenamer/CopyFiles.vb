809c53807eb50c08166dbfba2d14f15bc8198bbf
Matt <60934578+matthaeus206@users.noreply.github.com>
2024-01-16 08:23:15 -0800

    Update CopyFiles.vb

--- RA Macros/BulkRenamer/CopyFiles.vb
+++ RA Macros/BulkRenamer/CopyFiles.vb
@@ -1,82 +1,55 @@
 Sub CopyFiles()
     Dim sourceFolder As String
     Dim destFolder As String
-    Dim rng As Range
-    Dim Cell As Range
-    Dim fileName As String
-    Dim i As Long
-    Dim notFoundList As String
     Dim fileExtension As String
-    
-    ' Get input values from user with error handling
-    On Error Resume Next
-    sourceFolder = InputBox("Enter source folder path:")
-    destFolder = InputBox("Enter destination folder path:")
-    fileExtension = InputBox("Enter file extension:")
-    On Error GoTo 0
-    
-    ' Check for InputBox cancellation
-    If sourceFolder = "" Or destFolder = "" Or fileExtension = "" Then
-        MsgBox "Input cancelled. Exiting the procedure."
-        Exit Sub
-    End If
-    
-    ' Get range from user
-    On Error Resume Next
-    Set rng = Application.InputBox("Select cells with search terms:", Type:=8)
-    On Error GoTo 0
-    
-    ' Check if range is set
-    If rng Is Nothing Then
-        MsgBox "Range not selected. Exiting the procedure."
-        Exit Sub
-    End If
-    
-    ' Create FileSystemObject
-    Dim fso As Object
-    Set fso = CreateObject("Scripting.FileSystemObject")
-    
-    ' Disable alerts to prevent popups
-    Application.DisplayAlerts = False
-    
-    ' Loop through each cell in selected range
-    For Each Cell In rng
-        ' Loop through files in source folder
-        Debug.Print "Current Cell Value: " & Cell.Value
-        fileName = Dir(sourceFolder & "\*" & Cell.Value & "*" & fileExtension)
-        Debug.Print "Full File Path: " & sourceFolder & "\" & fileName
-        
-        If fileName = "" Then
-            ' File not found, add to not found list
-            notFoundList = notFoundList & Cell.Value & vbCrLf
+    Dim userRange As Range
+    Dim cell As Range
+    Dim fileNamePrefix As String
+    Dim errorList As Collection
+    Dim errorsSheet As Worksheet
+    Dim destinationPath As String
+    Dim sourceFile As String
+    Dim destFile As String
+    Dim foundFile As Boolean
+
+    ' Input source folder, destination folder, file extension, and user-defined range
+    sourceFolder = InputBox("Enter the source folder path:")
+    destFolder = InputBox("Enter the destination folder path:")
+    fileExtension = InputBox("Enter the file extension (e.g., txt, xls, xlsx):")
+    Set userRange = Application.InputBox("Select the user-defined range:", Type:=8)
+
+    ' Create a collection to store error file names
+    Set errorList = New Collection
+
+    ' Loop through each cell in the user-defined range
+    For Each cell In userRange
+        ' Get the first 5 digits of the file name
+        fileNamePrefix = Left(cell.Value, 5)
+
+        ' Construct the full path of the source file
+        sourceFile = sourceFolder & "\" & fileNamePrefix & "." & fileExtension
+
+        ' Construct the full path of the destination file
+        destFile = destFolder & "\" & fileNamePrefix & "." & fileExtension
+
+        ' Check if the file exists in the source folder
+        If Dir(sourceFile) <> "" Then
+            ' File found, copy to destination
+            FileCopy sourceFile, destFile
         Else
-            ' Copy file to destination folder
-            fso.CopyFile sourceFolder & "\" & fileName, destFolder & "\" & fileName
-            i = i + 1
+            ' File not found, add to error list
+            errorList.Add fileNamePrefix
         End If
-        Do While fileName <> ""
-            fileName = Dir()
-        Loop
-    Next Cell
-    
-    ' Enable alerts
-    Application.DisplayAlerts = True
-    
-    ' Display message with number of files copied
-    MsgBox i & " file(s) copied."
-    
-    ' Display list of search terms not found
-    If Len(notFoundList) > 0 Then
-        ' Create new sheet
-        Dim ws As Worksheet
-        Set ws = ThisWorkbook.Sheets.Add(After:=ThisWorkbook.Sheets(ThisWorkbook.Sheets.Count))
-        ws.Name = "Not Found"
-        
-        ' Display not found list in new sheet
-        ws.Range("A1").Value = "The following search terms were not found:"
-        ws.Range("A2").Value = notFoundList
-    End If
-    
-    ' Release FileSystemObject
-    Set fso = Nothing
+    Next cell
+
+    ' Create a new worksheet for errors
+    Set errorsSheet = Worksheets.Add
+    errorsSheet.Name = "Errors Found"
+
+    ' Paste the error file names in the new worksheet
+    For i = 1 To errorList.Count
+        errorsSheet.Cells(i, 1).Value = errorList(i)
+    Next i
+
+    MsgBox "Copying files completed. Check 'Errors Found' sheet for any errors.", vbInformation
 End Sub
