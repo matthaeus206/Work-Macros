de00fbead253d53c95af5a96da1f7cf576f0f224
Matt <60934578+matthaeus206@users.noreply.github.com>
2024-01-29 16:45:08 -0800

    Update RenameAllExtensions.bat

--- RenameAllExtensions.bat
+++ RenameAllExtensions.bat
@@ -1,4 +1,8 @@
 @echo off
+echo ----------------------------------------
+echo      Batch File Extension Changer
+echo      Make sure you select the correct directory before running
+echo ----------------------------------------
 set /p "directory=Enter the directory path: "
 set /p "oldExt=Enter the old extension (without dot): "
 set /p "newExt=Enter the new extension (without dot): "
@@ -6,5 +10,7 @@ set /p "newExt=Enter the new extension (without dot): "
 cd /d %directory%
 ren *.%oldExt% *.%newExt%
 
+echo.
 echo File extensions changed successfully.
+echo.
 pause
