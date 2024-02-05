db0d1e92e75d511dc5780a3089256b3054f19b44
Matt <60934578+matthaeus206@users.noreply.github.com>
2024-02-05 07:13:54 -0800

    Update RenameAllExtensions.bat
    added more verbose instructions

--- RenameAllExtensions.bat
+++ RenameAllExtensions.bat
@@ -1,10 +1,16 @@
 @echo off
 echo ----------------------------------------
 echo      Batch File Extension Changer
+echo	  USE FOR CONVERTING JPG, PNG ETC TO .1 FILES FOR RELEX
+echo
 echo      Make sure you select the correct directory before running
+echo      
 echo ----------------------------------------
+echo	  Select Correct Directory
 set /p "directory=Enter the directory path: "
+echo 	  Enter the old Extension e.g. "jpg, png. &c"
 set /p "oldExt=Enter the old extension (without dot): "
+echo	  Enter the new Extension e.g. "1" for relex graphics
 set /p "newExt=Enter the new extension (without dot): "
 
 cd /d %directory%
