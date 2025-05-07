f4ccfea74b30995414df6e8e98763ac975fb13f8
Matt <60934578+matthaeus206@users.noreply.github.com>
2025-05-07 08:46:08 -0700

    Update RenameFiles.vb
    Fixed potential crashes.
    - If a file is in use or locked, the script will crash.
    - Instead of CreateObject, declare it so it's easier to read and catch issues.

--- RA Macros/BulkRenamer/RenameFiles.vb
+++ RA Macros/BulkRenamer/RenameFiles.vb
@@ -1,42 +1,36 @@
 Sub RenameFiles()
-    Dim files As Object
-    Dim file As Object
-    Dim currentName As String
-    Dim newName As String
-    
-    'Prompt the user to enter the directory path using an input box
-    Dim folderPath As String
-    folderPath = InputBox("Enter the directory path where the files are located:")
-    
-    'Exit the subroutine if the user clicks the cancel button on the input box
-    If folderPath = "" Then Exit Sub
+    Dim fso As Object, files As Object, file As Object
+    Dim currentName As String, newName As String
+    Dim folderPath As String, findText As String, replaceText As String
 
-    'Prompt the user to enter the text to find in the file names using an input box
-    Dim findText As String
+    folderPath = InputBox("Enter the directory path where the files are located:")
+    If folderPath = "" Then Exit Sub
+    If Right(folderPath, 1) <> "\" Then folderPath = folderPath & "\"
+
     findText = InputBox("Enter the text to find in the file names:")
-    
-    'Exit the subroutine if the user clicks the cancel button on the input box
     If findText = "" Then Exit Sub
-    
-    'Prompt the user to enter the text to replace with in the file names using an input box
-    Dim replaceText As String
+
     replaceText = InputBox("Enter the text to replace with in the file names:")
-    
-    'Get all files in the directory
-    Set files = CreateObject("Scripting.FileSystemObject").GetFolder(folderPath).Files
-    
-    'Loop through each file and rename it
+
+    Set fso = CreateObject("Scripting.FileSystemObject")
+    Set files = fso.GetFolder(folderPath).Files
+
     For Each file In files
         currentName = file.Name
-        
-        'Remove the findText from the currentName if replaceText is blank
-        If replaceText = "" Then
-            newName = Replace(currentName, findText, "")
-        Else
-            newName = Replace(currentName, findText, replaceText)
+        newName = Replace(currentName, findText, replaceText)
+
+        If currentName <> newName Then
+            If Dir(folderPath & newName) = "" Then
+                On Error Resume Next
+                Name folderPath & currentName As folderPath & newName
+                If Err.Number <> 0 Then
+                    MsgBox "Failed to rename " & currentName & ": " & Err.Description, vbExclamation
+                    Err.Clear
+                End If
+                On Error GoTo 0
+            Else
+                MsgBox "Skipping rename: " & newName & " already exists.", vbExclamation
+            End If
         End If
-        
-        'Rename the file
-        Name folderPath & "\" & currentName As folderPath & "\" & newName
     Next file
 End Sub
