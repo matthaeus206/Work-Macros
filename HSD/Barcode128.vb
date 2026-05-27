db214a680528018093cf426d975fcfd44b2824d9
Matt <60934578+matthaeus206@users.noreply.github.com>
2026-05-27 11:15:18 -0700

    Update Barcode128.vb
    Fix Libre Barcode 128 checksum rendering for value 0
    
    - Fixed Code 128 glyph mapping issue where checksum value 0 rendered
      as a literal space character in Libre Barcode 128
    - Added explicit handling for Code 128 value 0 using ChrW(194)
      instead of ChrW(32)
    - Prevented barcode gaps caused by checksum spaces
    - Refactored start/stop character generation to consistently use
      Code128Glyph()
    - Improved validation and error messaging
    - Added empty input validation
    - Cleaned up variable naming and encoding flow

--- HSD/Barcode128.vb
+++ HSD/Barcode128.vb
@@ -2,14 +2,17 @@ Option Explicit
 
 Private Function Code128Glyph(ByVal value As Long) As String
     If value < 0 Or value > 106 Then
-        Err.Raise vbObjectError + 514, , "Invalid Code 128 value"
+        Err.Raise vbObjectError + 514, , "Invalid Code 128 value: " & value
     End If
 
-    If value <= 94 Then
-        Code128Glyph = ChrW(value + 32)
-    Else
-        Code128Glyph = ChrW(value + 100)
-    End If
+    Select Case value
+        Case 0
+            Code128Glyph = ChrW(194)   ' Libre Barcode 128: avoid literal space
+        Case 1 To 94
+            Code128Glyph = ChrW(value + 32)
+        Case 95 To 106
+            Code128Glyph = ChrW(value + 100)
+    End Select
 End Function
 
 Public Function Code128B(ByVal inputText As String) As String
@@ -17,31 +20,33 @@ Public Function Code128B(ByVal inputText As String) As String
     Dim checksum As Long
     Dim charValue As Long
     Dim encoded As String
+    Dim currentChar As String
+
+    If Len(inputText) = 0 Then
+        Err.Raise vbObjectError + 512, , "Input text cannot be blank"
+    End If
 
     ' Start Code B = 104
     checksum = 104
-    encoded = ChrW(204)
+    encoded = Code128Glyph(104)
 
     For i = 1 To Len(inputText)
-        charValue = AscW(Mid$(inputText, i, 1)) - 32
+        currentChar = Mid$(inputText, i, 1)
+        charValue = AscW(currentChar) - 32
 
         If charValue < 0 Or charValue > 95 Then
-            Err.Raise vbObjectError + 513, , "Invalid character for Code 128 B"
+            Err.Raise vbObjectError + 513, , _
+                "Invalid character for Code 128 B: " & currentChar
         End If
 
         checksum = checksum + (charValue * i)
-
-        ' Code Set B data characters
         encoded = encoded & Code128Glyph(charValue)
     Next i
 
     checksum = checksum Mod 103
 
-    ' Checksum character
     encoded = encoded & Code128Glyph(checksum)
-
-    ' Stop character = 106
-    encoded = encoded & ChrW(206)
+    encoded = encoded & Code128Glyph(106)
 
     Code128B = encoded
 End Function
